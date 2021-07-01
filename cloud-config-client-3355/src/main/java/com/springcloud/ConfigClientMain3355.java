391bc089aea76bcd3c780c1204f6182a79e8babf
liling110 <ly0106998@163.com>
2021-07-01 17:13:57 +0800

    commit 3355

--- cloud-config-client-3355/src/main/java/com/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/springcloud/ConfigClientMain3355.java
@@ -1,2 +1,13 @@
-package com.springcloud;public class ConfigClientMain3355 {
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
 }
